d902a3f1a6cdca1dfe19581853a429404d626b82
Sergio Nieto <snieto10@gmail.com>
2022-12-05 15:46:13 -0500

    progress 1

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,41 +1,46 @@
-import userEvent from "@testing-library/user-event";
 import home from "../assets/icons/home.svg";
 import user from "../assets/icons/user.svg";
 import phone from "../assets/icons/phone.svg";
-import aboutme from "../assets/icons/aboutme.svg";
+import { Link } from "react-router-dom";
 
 function Menu() {
   return (
     <>
       <div className="container-flexStart menu_box">
-        <div className="menu_button container-flex">
-          <div className="container-flex menu_icon">
-            <img className="icon_size" src={home} alt="home logo" />
+        <Link to="/">
+          <div className="menu_button container-flex">
+            <div className="container-flex menu_icon">
+              <img className="icon_size" src={home} alt="home logo" />
+            </div>
+            <div className="container-flex menu_text">
+              <h3 className="menu_h3">HOME</h3>
+            </div>
           </div>
-          <div className="container-flex menu_text">
-            <h3 className="menu_h3">HOME</h3>
-          </div>
-        </div>
+        </Link>
       </div>
       <div className="container-flexStart menu_box">
-        <div className="menu_button container-flex">
-          <div className="container-flex menu_icon">
-            <img src={user} alt="skills" />
+        <Link to="/skills">
+          <div className="menu_button container-flex">
+            <div className="container-flex menu_icon">
+              <img src={user} alt="skills" />
+            </div>
+            <div className="container-flex menu_text">
+              <h3 className="menu_h3">SKILLS</h3>
+            </div>
           </div>
-          <div className="container-flex menu_text">
-            <h3 className="menu_h3">SKILLS</h3>
-          </div>
-        </div>
+        </Link>
       </div>
       <div className="container-flexStart menu_box">
-        <div className="menu_button container-flex">
-          <div className="container-flex menu_icon size_phone">
-            <img src={phone} alt="contact" />
+        <Link to="/contact">
+          <div className="menu_button container-flex">
+            <div className="container-flex menu_icon size_phone">
+              <img src={phone} alt="contact" />
+            </div>
+            <div className="container-flex menu_text">
+              <h3 className="menu_h3">CONTACT</h3>
+            </div>
           </div>
-          <div className="container-flex menu_text">
-            <h3 className="menu_h3">CONTACT</h3>
-          </div>
-        </div>
+        </Link>
       </div>
     </>
   );
--- src/layout/center.jsx
+++ src/layout/center.jsx
@@ -1,5 +1,18 @@
+import { Route, Routes } from "react-router-dom";
+import Home from "../components/Home";
+import Skills from "../components/Skills";
+import Contact from "../components/Contact";
+
 function Center() {
-  return <div>Center</div>;
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/skills" element={<Skills />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </>
+  );
 }
 
 export default Center;
