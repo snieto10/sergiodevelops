6cc2ea562ffae07b428954c9e85b2368614d62a1
Sergio Nieto <snieto10@gmail.com>
2022-12-07 10:25:49 -0500

    like is working

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,114 +1,12 @@
+import { faThumbsUp } from "@fortawesome/free-solid-svg-icons";
 import React from "react";
-import as from "../assets/images/as.jpg";
+import Website1 from "../websites/Website1";
 
 function Home() {
   return (
     <>
       <div className="grid_home">
-        <div className="grid_block">
-          <div className="">
-            <img src={as} alt="sport page" />
-          </div>
-          <div className="div">
-            <h1 className="section">Sports News Site</h1>
-            <h3 className="section2">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet
-              excepturi obcaecati tempora similique vitae. Quibusdam incidunt
-              asperiores iste sint ea!
-            </h3>
-          </div>
-          <div className="container-media">
-            <div className="">Like</div>
-            <div className="">Comment</div>
-            <div className="">GitHub</div>
-          </div>
-        </div>
-        <div className="grid_block">
-          <div className="">
-            <img src={as} alt="sport page" />
-          </div>
-          <div className="div">
-            <h1 className="section">Sports News Site</h1>
-            <h3 className="section2">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet
-              excepturi obcaecati tempora similique vitae. Quibusdam incidunt
-              asperiores iste sint ea!
-            </h3>
-          </div>
-          <div className="container-media">
-            <div className="">Like</div>
-            <div className="">Comment</div>
-            <div className="">GitHub</div>
-          </div>
-        </div>
-        <div className="grid_block">
-          <div className="">
-            <img src={as} alt="sport page" />
-          </div>
-          <div className="div">
-            <h1 className="section">Sports News Site</h1>
-            <h3 className="section2">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet
-              excepturi obcaecati tempora similique vitae. Quibusdam incidunt
-              asperiores iste sint ea!
-            </h3>
-          </div>
-          <div className="container-media">
-            <div className="">Like</div>
-            <div className="">Comment</div>
-            <div className="">GitHub</div>
-          </div>
-        </div>
-        <div className="grid_block">
-          <div className="">
-            <img src={as} alt="sport page" />
-          </div>
-          <div className="div">
-            <h1 className="section">Sports News Site</h1>
-            <h3 className="section2">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet
-              excepturi obcaecati tempora similique vitae. Quibusdam incidunt
-              asperiores iste sint ea!
-            </h3>
-          </div>
-          <div className="container-media">
-            <div className="">Like</div>
-            <div className="">Comment</div>
-            <div className="">GitHub</div>
-          </div>
-        </div>
-        <div className="grid_block">
-          <div className="">
-            <img src={as} alt="sport page" />
-          </div>
-          <div className="div">
-            <h1 className="section">Sports News Site</h1>
-            <h3 className="section2">Lorem ipsum dolor</h3>
-          </div>
-          <div className="container-media">
-            <div className="">Like</div>
-            <div className="">Comment</div>
-            <div className="">GitHub</div>
-          </div>
-        </div>
-        <div className="grid_block">
-          <div className="">
-            <img src={as} alt="sport page" />
-          </div>
-          <div className="div">
-            <h1 className="section">Sports News Site</h1>
-            <h3 className="section2">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet
-              excepturi obcaecati tempora similique vitae. Quibusdam incidunt
-              asperiores iste sint ea!
-            </h3>
-          </div>
-          <div className="container-media">
-            <div className="">Like</div>
-            <div className="">Comment</div>
-            <div className="">GitHub</div>
-          </div>
-        </div>
+        <Website1 />
       </div>
     </>
   );
---
+++ src/websites/Website1.jsx
@@ -0,0 +1,62 @@
+import React from "react";
+import as from "../assets/images/as.jpg";
+import like from "../assets/icons/thumbs-up-solid.svg";
+import comment from "../assets/icons/comment-solid.svg";
+import github from "../assets/icons/square-github.svg";
+import { useState } from "react";
+
+function Website1() {
+  const [likes, setLikes] = useState(3);
+  const [text, setText] = useState(["Great Website", "Love the colors"]);
+  const [likeIsDisabled, setLikeIsDisabled] = useState(false);
+
+  const handleLike = () => {
+    setLikes(likes + 1);
+    setLikeIsDisabled(true);
+  };
+  return (
+    <>
+      <div className="grid_block">
+        <div className="">
+          <img src={as} alt="sport page" />
+        </div>
+        <div className="div">
+          <h1 className="section">Sports News Site</h1>
+          <h3 className="section2">Lorem</h3>
+        </div>
+        <div className="container-media">
+          <div className="">
+            <button
+              onClick={handleLike}
+              disabled={likeIsDisabled}
+              className="container-flex noBtn"
+            >
+              <img
+                src={like}
+                className={likeIsDisabled ? "icon icon-disabled" : "icon"}
+                alt=""
+              />
+              <h3 className="icon_text">{likes}</h3>
+            </button>
+          </div>
+          <div className="">
+            <div className="container-flex">
+              <img src={comment} className="icon" alt="" />
+              <h3 className="icon_text">{text.length}</h3>
+            </div>
+          </div>
+          <div className="">
+            <a href="https://youtube.com/" target="_blank">
+              <div className="container-flex">
+                <img src={github} className="icon" alt="" />
+                <p className="icon_text">View Code</p>
+              </div>
+            </a>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Website1;
